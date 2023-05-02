18352279ebb670807c7224023a28919127aae40e
xxjingyi <xxjingyi@outlook.com>
2023-05-02 17:37:12 +0800

    upstate

--- tools/calculater/calculator.js
+++ tools/calculater/calculator.js
@@ -1,11 +1,28 @@
 let output = document.getElementById('output');
+let result;
 
 function appendToOutput(value) {
   output.value += value;
 }
 
-function clearOutput() {
+function clearScreen() {
   output.value = '';
+}
+
+function saveResult() {
+  result = eval(output.value);
+  alert(`结果 ${result} 已记录`);
+}
+
+function recallResult() {
+  if (result != null) {
+    output.value += result;
+  }
+}
+
+function deleteLastInput() {
+  let currentValue = output.value;
+  output.value = currentValue.substring(0, currentValue.length - 1);
 }
 
 function calculate() {
@@ -13,6 +30,6 @@ function calculate() {
     output.value = eval(output.value);
   } catch (error) {
     alert('无效的计算表达式');
-    clearOutput();
+    clearScreen();
   }
 }
